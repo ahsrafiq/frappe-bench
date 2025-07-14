7c3c3d67ce6297ecf9e3ffd5e01898ecaa804a95
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2025-07-15 02:03:41 +0530

    Added frappe and erpnext as submodules

--- apps/frappe/frappe/patches/v13_0/eol_warning.py
+++
@@ -1,9 +0,0 @@
-import click
-
-
-def execute():
-	click.secho(
-		"Version 13 of Frappe Framework is no longer supported.\n"
-		"Consider upgrading to latest version to recieve regular fixes and new features.",
-		fg="red",
-	)
